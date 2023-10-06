192b27fc20e33b6610d80fabed0120cae0ad390e
Julio D. Chavez <chavezjulio67@gmail.com>
2023-10-06 00:35:55 -0500

    Practicing designs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,11 +4,13 @@ const Button = ({title, onClick}) => {
   return (
     <div>
       <button
+        className = "button"
         style={{
           maxWidth: "140px",
           minWidth: "80px",
           height: "30px",
-          marginRight: "5px"
+          marginRight: "5px",
+          borderRadius: "10px",
         }}
         onClick={onClick}
       >
